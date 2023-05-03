7643796bc02ab88aa51a23fb369b43cfa78cd684
endritti <es47431@ubt-uni.net>
2023-05-04 00:31:19 +0200

    Added studenti backend and made first crud

---
+++ Lab 2 Project/studenti/Controllers/NxenesiController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using studenti.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studenti.Controllers
+{
+    [Route("admin/[controller]")]
+    [ApiController]
+    public class NxenesiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public NxenesiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"select nxenesiID,emri_mbiemri,email,passwordi,fotoPath,vendbanimi,nrTelefonit,drejtimi,emriPrindit,prindiID from dbo.nxenesi";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DBAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Nxenesi nx)
+        {
+            string query = @"insert into dbo.nxenesi values
+                            ('" + nx.emri_mbiemri + @"',
+                            '" + nx.email + @"',
+                            '" + nx.passwordi + @"',
+                            '" + nx.fotoPath + @"',
+                            '" + nx.vendbanimi + @"',
+                            '" + nx.nrTelefonit + @"',
+                            '" + nx.drejtimi + @"',
+                            '" + nx.emriPrindit + @"',
+                            '" + nx.prindiID + @"'),                        
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DBAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Nxenesi nx)
+        {
+            string query = @"update dbo.nxenesi set 
+                            emri_mbiemri = '" + nx.emri_mbiemri + @"',
+                            email = '" + nx.email + @"',
+                            passwordi = '" + nx.passwordi + @"',
+                            fotoPath = '" + nx.fotoPath + @"'                         
+                            vendbanimi = '" + nx.vendbanimi + @"',
+                            nrTelefonit = '" + nx.nrTelefonit + @"',
+                            drejtimi = '" + nx.drejtimi + @"',
+                            emriPrindit = '" + nx.emriPrindit + @"',
+                            emriPrindit = '" + nx.emriPrindit + @"',
+
+                            where nxenesiID = " + nx.nxenesiID + @"
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DBAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from dbo.nxenesi 
+                            where nxenesiID = " + id + @"
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DBAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
